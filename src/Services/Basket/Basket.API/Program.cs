543b50a3eb1ba6ce82584b2bacc3eff2d2bb2d01
Abdallah Mohamed El-Zohiry <abdallamohamd517@gmail.com>
2024-05-10 00:04:26 +0300

    Add CRUD and Repository to Basket API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,12 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var assembly = typeof(Program).Assembly;
+var ConnectionString = builder.Configuration.GetConnectionString("Database")!;
 // Add DI
 
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(ConnectionString);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+builder.Services.AddCarter();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
 
 // Configure the HTTP Request Pipeline.
 
-
+app.MapCarter();
+app.UseExceptionHandler(opt => { });
 app.Run();
