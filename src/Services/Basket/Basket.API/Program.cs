aff3688c00feab51fc8f1c099bc97728fe27e02c
Abdallah Mohamed El-Zohiry <abdallamohamd517@gmail.com>
2024-05-11 02:53:49 +0300

    Add Basket Api to Docket

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
 var ConnectionString = builder.Configuration.GetConnectionString("Database")!;
+var CacheConnectionString = builder.Configuration.GetConnectionString("Redis")!;
 // Add DI
 
 builder.Services.AddMediatR(config =>
@@ -19,12 +20,24 @@ builder.Services.AddCarter();
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(opt =>
+{
+    opt.Configuration = CacheConnectionString;
+});
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(ConnectionString)
+    .AddRedis(CacheConnectionString);
 
 var app = builder.Build();
 
 
 // Configure the HTTP Request Pipeline.
-
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 app.MapCarter();
 app.UseExceptionHandler(opt => { });
 app.Run();
