af84a6b99baf18a76faa91d457d76e2932c5b37b
Abdallah Mohamed El-Zohiry <abdallamohamd517@gmail.com>
2024-05-13 03:09:42 +0300

    Add Discount.Grpc to Docker

--- src/Services/Discount/Discount.Grpc/GlobalUsing.cs
+++ src/Services/Discount/Discount.Grpc/GlobalUsing.cs
@@ -1,5 +1,7 @@
 ﻿global using Discount.Grpc.Data;
+global using Discount.Grpc.Models;
 global using Discount.Grpc.Protos;
 global using Discount.Grpc.Services;
 global using Grpc.Core;
+global using Mapster;
 global using Microsoft.EntityFrameworkCore;
